1d3183bb9d76fbc62c67d927e3dd05773bef5d6e
Krishan <33421343+kfiven@users.noreply.github.com>
2022-06-21 11:45:32 +0530

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,16 +3,8 @@
   windows_subsystem = "windows"
 )]
 
-#[cfg(target_os = "macos")]
-mod menu;
-
 fn main() {
-  let builder = tauri::Builder::default();
-
-  #[cfg(target_os = "macos")]
-  let builder = builder.menu(menu::menu());
-
-  builder
+  tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
