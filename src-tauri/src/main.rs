d32c0426b6538fe81a02edc96e2f062f8abdf922
Krishan <33421343+kfiven@users.noreply.github.com>
2025-08-07 21:09:47 +1000

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,31 +2,31 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+
 #[cfg(target_os = "macos")]
 mod menu;
-mod tray;
 
 fn main() {
-    let builder = tauri::Builder::default();
+    let port = 44548;
+
+    let context = tauri::generate_context!();
+    let builder = tauri::Builder::default()
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_os::init());
 
     #[cfg(target_os = "macos")]
     let builder = builder.menu(menu::menu());
 
-    let builder = builder
-        .system_tray(tray::system_tray())
-        .on_system_tray_event(tray::system_tray_handler);
-
     builder
-        .build(tauri::generate_context!())
+        .plugin(tauri_plugin_localhost::Builder::new(port).build())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .run(context)
         .expect("error while building tauri application")
-        .run(run_event_handler)
-}
-
-fn run_event_handler<R: tauri::Runtime>(app: &tauri::AppHandle<R>, event: tauri::RunEvent) {
-    match event {
-        tauri::RunEvent::WindowEvent { label, event, .. } => {
-            tray::window_event_handler(app, &label, &event);
-        }
-        _ => {}
-    }
 }
