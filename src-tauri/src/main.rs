260286d9325b8cff5d86c789014c2ad8259a9b91
Krishan <33421343+kfiven@users.noreply.github.com>
2025-08-11 11:40:54 +1000

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,27 +6,27 @@
 #[cfg(target_os = "macos")]
 mod menu;
 
+use tauri::{utils::config::AppUrl, WindowUrl};
+
 fn main() {
     let port = 44548;
 
-    let context = tauri::generate_context!();
-    let builder = tauri::Builder::default()
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-        .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_os::init());
+    let mut context = tauri::generate_context!();
+    let url = format!("http://localhost:{}", port).parse().unwrap();
+    let window_url = WindowUrl::External(url);
+    
+    // Rewrite the config so the IPC is enabled on this URL
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+    context.config_mut().build.dev_path = AppUrl::Url(window_url.clone());
+
+    let builder = tauri::Builder::default();
 
     #[cfg(target_os = "macos")]
-    let builder = builder.menu(menu::menu());
+    let builder = builder.setup(menu::menu());
 
     builder
         .plugin(tauri_plugin_localhost::Builder::new(port).build())
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .run(context)
-        .expect("error while building tauri application")
+        .expect("error while building tauri application");
 }
