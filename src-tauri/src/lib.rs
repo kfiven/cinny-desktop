159ae795ed1b8a82616f2cea1a7933f9b6dd58fb
Krishan <33421343+kfiven@users.noreply.github.com>
2025-09-07 23:36:13 +1000

    add mod menu

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,33 +3,8 @@
     windows_subsystem = "windows"
 )]
 
-// #[cfg(target_os = "macos")]
-// mod menu;
-
-// use tauri::{webview::WebviewWindowBuilder, WebviewUrl};
-
-// fn main() {
-//     let port: u16 = 44548;
-
-//     let mut context = tauri::generate_context!();
-//     let url = format!("http://localhost:{}", port).parse().unwrap();
-//     let window_url = WebviewUrl::External(url);
-//     // context.config_mut().build.frontend_dist = WebviewWindowBuilder::Url(window_url.clone());
-//     // context.config_mut().build.dev_url = WebviewWindowBuilder::Url(window_url.clone());
-
-//     context.config_mut().build.frontend_dist = WebviewWindowBuilder::new(app, "Cinny", window_url.clone());
-//     context.config_mut().build.dev_url = WebviewWindowBuilder::new(app, "Cinny", window_url.clone());
-//     let builder = tauri::Builder::default();
-
-//     #[cfg(target_os = "macos")]
-//     let builder = builder.menu(menu::menu());
-
-//     builder
-//         .plugin(tauri_plugin_localhost::Builder::new(port).build())
-//         .plugin(tauri_plugin_window_state::Builder::default().build())
-//         .run(context)
-//         .expect("error while building tauri application")
-// }
+#[cfg(target_os = "macos")]
+mod menu;
 
 use tauri::{webview::WebviewWindowBuilder, WebviewUrl};
 
@@ -58,4 +33,3 @@ pub fn run() {
         .run(context)
         .expect("error while running tauri application");
 }
-
