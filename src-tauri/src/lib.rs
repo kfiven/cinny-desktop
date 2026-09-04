f54156379c758e5a69c758a22f4b56f28a95f871
kfiven <33421343+kfiven@users.noreply.github.com>
2026-09-04 06:18:14 +0000

    chore: add SPDX headers

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,9 @@
 #![cfg_attr(
+// Cinny Project
+// Copyright (c) 2021-2026 Ajay Bura
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// https://cinny.in
+
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Cinny Project
+// Copyright (c) 2021-2026 Ajay Bura
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// https://cinny.in
+
 
 fn main() {
   app_lib::run();
