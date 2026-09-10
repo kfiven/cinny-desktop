a651106b81a14bc6a11101d55c48988dad3bea28
kfiven <33421343+kfiven@users.noreply.github.com>
2026-09-10 14:16:42 +0000

    chore: add SPDX headers

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,11 @@
+/*
+Cinny Project
+Copyright 2021-present Ajay Bura
+SPDX-License-Identifier: AGPL-3.0-only
+https://cinny.in
+
+Please see LICENSE files in the repository root for full details.*/
+
 fn main() {
     tauri_build::build()
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,11 @@
+/*
+Cinny Project
+Copyright 2021-present Ajay Bura
+SPDX-License-Identifier: AGPL-3.0-only
+https://cinny.in
+
+Please see LICENSE files in the repository root for full details.*/
+
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
